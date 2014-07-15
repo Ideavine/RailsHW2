2e393f7f6257a45ca4ee23019f0b8ae112bb35ec
Virginia Dooley <virginia@ideavine.org>
2014-07-15 16:41:39 -0400

    -a

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@
 
 
 Rails.application.routes.draw do 
-	#I want this first route to show the view index.html.erb but the error message tells me Missing template home/index, application/index with {:locale=>[:en], :formats=>[:html], :variants=>[], :handlers=>[:erb, :builder, :raw, :ruby, :jbuilder, :coffee]}. Searched in: * "/Users/VirginiaDooley/Documents/NYCDA/NYCDA-Assignments/RoRHW1/RoRHW1files/app/views"
 	get '/' => "home#index"
 	get '/index' => "home#index"
 	get '/join' => "home#join"
