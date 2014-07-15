47b7fec6a96e7c543757a29a2cd86592b30d2e06
Virginia Dooley <virginia@ideavine.org>
2014-07-15 14:31:29 -0400

    -a

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,14 @@
+class HomeController < ApplicationController
+	def index
+		# @title = "Home"
+	end
+
+
+	def contact
+		# @title = "Contact"
+	end
+end
+
+# def alternate
+	# 	@id = params[:id]
+	# 	layout 'alternate'
---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+# The generic format is like so:
+# HTTPVERB ‘URL’ => 
+# “CONTROLLER#VIEW”
+
+
+Rails.application.routes.draw do 
+	#I want this first route to show the view index.html.erb but the error message tells me Missing template home/index, application/index with {:locale=>[:en], :formats=>[:html], :variants=>[], :handlers=>[:erb, :builder, :raw, :ruby, :jbuilder, :coffee]}. Searched in: * "/Users/VirginiaDooley/Documents/NYCDA/NYCDA-Assignments/RoRHW1/RoRHW1files/app/views"
+	get '/' => "home#index"
+	get '/index' => "home#index"
+	get '/join' => "home#join"
+ 	get '/contact' => "home#contact"
+ 	get '/contact/:id' => "home#contact", as: :contactus
+ 	get '/success' => "user#contact", as: :success
+ 	get '/error' => "user#contact", as: :error
+end
+
+#where do i put this? 
+# link_to “Our Homepage”, home_path
+
+
+
+
